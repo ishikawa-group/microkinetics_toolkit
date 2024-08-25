b1aee8504c4ad04a16d14b227298ce1117fa9b30
ISHIKAWA Atsushi <ishikawa.a.ai@m.titech.ac.jp>
2024-08-26 02:49:46 +0900

    update

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="microkinetics_toolkit",
+    version="0.0",
+    description="for microkinetics",
+    author="Atsushi Ishikawa",
+    author_email="ishikawa.a.ai@m.titech.ac.jp",
+    url="",
+)
