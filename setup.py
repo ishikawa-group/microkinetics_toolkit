65791173ffb3b3300a0701e1df4f4268bd19bac5
ISHIKAWA Atsushi <ishikawa.a.ai@m.titech.ac.jp>
2024-09-19 01:54:16 +0900

    updated

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup
+from setuptools import find_packages
 
 setup(
     name="microkinetics_toolkit",
@@ -7,4 +8,5 @@ setup(
     author="Atsushi Ishikawa",
     author_email="ishikawa.a.ai@m.titech.ac.jp",
     url="",
+    packages=find_packages(include=["microkinetics_toolkit"]),
 )
